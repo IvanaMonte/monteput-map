667692b8991481441c23341a8c781f233f622438
Ivana <ivana.jovanovic@monteput.me>
2025-10-29 14:20:17 +0100

    url

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -278,7 +278,7 @@ if (activeSegment) {
 
           {/* QR kod */}
           <div className="absolute bottom-6 right-6 z-50 hidden md:block">
-            <QrCodeBox url="https://monteput-silk.vercel.app/" />
+            <QrCodeBox url="https://map-sigma-umber.vercel.app/" />
           </div>
         </div>
       </main>
